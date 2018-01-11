c5aee9963d0ca2e3d3df5d2003b6d4336995543c
Gina <ginamantonini@gmail.com>
2018-01-10 22:25:22 -0600

    added three classes

---
+++ FavoriteThings/Ramen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteThings
+{
+    class Ramen
+    {
+        protected string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+}

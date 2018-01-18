fc0cc826fbd0fbaae4c88909d66f1ce82b418f3d
Gina <ginamantonini@gmail.com>
2018-01-17 21:55:13 -0600

    added another favorite thing class

---
+++ FavoriteThings/Food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FavoriteThings.Favorites
+{
+    class Food
+    {
+
+        public string Cuisine { get; set; }
+        public bool EatWithHands { get; set; }
+       
+    }
+}
+

8f9e2cd54fbeed882e31df369c34b3705df53b3d
Gina <ginamantonini@gmail.com>
2018-01-18 19:38:54 -0600

    added list for each favorite thing class, added new instances of each favorite thing in appropriate list

--- FavoriteThings/Food.cs
+++ FavoriteThings/Food.cs
@@ -4,10 +4,9 @@ namespace FavoriteThings.Favorites
 {
     class Food
     {
-
         public string Cuisine { get; set; }
+        public string SampleDish { get; set; }
         public bool EatWithHands { get; set; }
-       
     }
 }
 
--- FavoriteThings/Program.cs
+++ FavoriteThings/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using FavoriteThings.Favorites;
+using System.Collections.Generic;
 
 namespace FavoriteThings
 {
@@ -7,29 +8,50 @@ namespace FavoriteThings
     {
         static void Main(string[] args)
         {
-            var puppy = new Puppy
+            var puppies = new List<Puppy>
             {
-                Breed = "German Shepherd",
-                Size = "Medium",
-                IsHypoallergenic = false
+                new Puppy {Breed = "German Shepherd", Size = "large", IsHypoallergenic = false},
+                new Puppy {Breed = "Golden Doodle", Size = "large", IsHypoallergenic = true},
+                new Puppy {Breed = "Mastiff", Size = "extra large", IsHypoallergenic = false}
             };
-            puppy.InducingSneezes(puppy.IsHypoallergenic);
 
-            var park = new Park
+            foreach (var puppy in puppies)
             {
-                Area = "East Nashville",
-                Name = "Two Rivers Waterpark",
-                OpenInWinter = false
-            };
-            park.WinterParkSearch(park.OpenInWinter);
+                Console.WriteLine($"{puppy.Breed} dogs are {(puppy.IsHypoallergenic ? "allergy safe" : "sneeze inducers")}");
+            }
 
-            var soup = new Soup
+            var parks = new List<Park>
             {
-                Type = "Vegetarian Chili", 
-                SpiceLevel = "Ghost pepper hot",
-                IsVegetarian = true
+                new Park {Area = "East Nashville", Name = "Two Rivers Waterpark", OpenInWinter = false},
+                new Park {Area = "Cumberland Plateau", Name = "Fiery Gizzard Trail", OpenInWinter = true},
+                new Park {Area = "East Nashville", Name = "Shelby Park", OpenInWinter = true}
             };
-            soup.VegetarianSafety(soup.IsVegetarian);
+
+            foreach (var park in parks)
+            {
+            }
+
+            var soups = new List<Soup>
+            {
+                new Soup {Type = "Vegetarian Chili", SpiceLevel = "Ghost pepper hot", IsVegetarian = true},
+                new Soup {Type = "Clam Chowder", SpiceLevel = "Not spicy", IsVegetarian = false},
+                new Soup {Type = "Tom Yum", SpiceLevel = "Medium", IsVegetarian = false}
+            };
+
+            foreach (var soup in soups)
+            {
+            }
+
+            var foods = new List<Food>
+            {
+                new Food {Cuisine = "Thai", SampleDish = "Pad Kra Prow", EatWithHands = false},
+                new Food {Cuisine = "Etiophian", SampleDish = "Injera", EatWithHands = true},
+                new Food {Cuisine = "Vietnamese", SampleDish = "Bahn Mi", EatWithHands = true}
+            };
+
+            foreach (var food in foods)
+            {
+            }
         }
 
     }
